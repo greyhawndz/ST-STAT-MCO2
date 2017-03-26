574823b522f9dc3897a676f8794d4d5a0009724f
William Dionio <wdionio@gmail.com>
2017-03-26 23:57:16 +0800

    Added log Hypergeometric

--- STAT MCO2/src/java/Model/Logger.java
+++ STAT MCO2/src/java/Model/Logger.java
@@ -25,6 +25,40 @@ public class Logger {
         logFile = new File(FILE_NAME);
     }
     
+    public void logHypergeometric(int experimentId, String objectExperiment, String experimentType, double numSuccess, double actualProbability, double idealProbability,  int totalPopulation, int successPopulation, int randomSample, int successSample, int expTrials) throws IOException{
+        sb = new StringBuilder();
+        if(!logFile.exists()){
+            logFile.createNewFile();
+        }
+        
+        FileWriter fw = new FileWriter(logFile.getAbsoluteFile(), true);
+        BufferedWriter bw = new BufferedWriter(fw);
+        sb.append(objectExperiment +" Experiment " +experimentId + ":");
+        sb.append(System.lineSeparator());
+        sb.append("Experiment Type: " +experimentType);
+        sb.append(System.lineSeparator());
+        sb.append("Number of experiment repetitions: " +expTrials);
+        sb.append(System.lineSeparator());
+        sb.append("Total Population: " +totalPopulation);
+        sb.append(System.lineSeparator());
+        sb.append("Successes in Population: " +successPopulation);
+        sb.append(System.lineSeparator());
+        sb.append("Sample Size: " +randomSample);
+        sb.append(System.lineSeparator());
+        sb.append("Sample Success: " +successSample);
+        sb.append(System.lineSeparator());
+        sb.append("Average Number of successes: " +numSuccess);
+        sb.append(System.lineSeparator());
+        sb.append("Actual Probability: " +actualProbability);
+        sb.append(System.lineSeparator());
+        sb.append("Ideal Probability: " +idealProbability);
+        sb.append(System.lineSeparator());
+        bw.write(sb.toString());
+        bw.newLine();
+        bw.close();
+        fw.close();
+    }
+    
     public void logExperiment(int experimentId, String objectExperiment, String experimentType, double numSuccess, double actualProbability, double idealProbability, int trials, int expTrials) throws IOException{
         sb = new StringBuilder();
         if(!logFile.exists()){
